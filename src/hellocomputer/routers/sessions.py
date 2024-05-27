06ac295e17fba4ea6c55b75284d3fdf567d4aaff
Guillem Borrell <borrell.guillem@bcg.com>
2024-05-27 10:02:55 +0200

    Trying to get authentication right

--- src/hellocomputer/routers/sessions.py
+++ src/hellocomputer/routers/sessions.py
@@ -1,13 +1,24 @@
 from uuid import uuid4
-
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
+
+from ..config import settings
+from ..security import oauth2_scheme
+# Scheme for the Authorization header
 
 router = APIRouter()
 
 
+if settings.auth:
+
+    @router.get("/token")
+    async def get_token() -> str:
+        return str(uuid4())
+
+
 @router.get("/new_session")
-async def get_new_session() -> str:
+async def get_new_session(token: Annotated[str, Depends(oauth2_scheme)]) -> str:
     return str(uuid4())
 
 
